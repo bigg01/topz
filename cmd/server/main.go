93622ef8daca6b92ebdb6ea50d61ace18ebd793b
bigg01 <o.guggenbuehl@gmail.com>
2018-10-11 15:44:53 +0200

    add env for bind

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 
 	"log"
+	"os"
 
 	"github.com/brendandburns/topz/pkg/topz"
 )
 
 func main() {
 	http.HandleFunc("/topz", topz.HandleTopz)
-
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	bindHttp := os.Getenv("TOPZ_BIND")
+	if bindHttp == "" {
+		bindHttp = "localhost:8080"
+		log.Printf("environment variable bindHttp not set using default: %v\n", bindHttp)
+	}
+	log.Fatal(http.ListenAndServe(bindHttp, nil))
 }
